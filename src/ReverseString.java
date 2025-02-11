bd5046ac630e71b288c56b1185d65454e382e4b6
suhaspaul96 <suhasp0403@gmail.com>
2025-02-11 21:59:42 +0530

    My fifth Commit

---
+++ src/ReverseString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+
+        String S = sc.next();
+        String rev = "";
+
+        for(int i=S.length()-1;i>=0;i--)
+        {
+            rev = rev + S.charAt(i);
+        }
+        System.out.println("Reverse String is: "+rev);
+    }
+}
