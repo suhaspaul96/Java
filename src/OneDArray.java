b92b11f72e0d97d73b08d1ba3501cb0d1729d564
suhaspaul96 <suhasp0403@gmail.com>
2025-02-12 00:29:18 +0530

    My sixth Commit

---
+++ src/OneDArray.java
@@ -0,0 +1,20 @@
+public class OneDArray {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+        numbers[0] = 97;
+        numbers[1] = 98;
+        numbers[2] = 99;
+
+        System.out.println("number at index: "+ numbers[1]);
+
+        String[] names = new String[3];
+
+        names[0] = "Suhas";
+        names[1] = "Dhaval";
+        names[2] = "Avanti";
+
+        System.out.println("name at index: "+ names[2]);
+    }
+}
