26e8e0ac7e84c14e24f2314fd8f88412b776eac9
suhaspaul96 <suhasp0403@gmail.com>
2025-02-12 22:38:49 +0530

    My eighth Commit

---
+++ src/ArrayLinearSearch.java
@@ -0,0 +1,27 @@
+public class ArrayLinearSearch {
+    static int search(int a[], int n, int x)
+    {
+        for (int i = 0; i < n; i++) {
+            if (a[i] == x)
+                return i;
+        }
+
+        // return -1 if the element is not found
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] a = { 3, 4, 1, 7, 5 };
+        int n = a.length;
+
+        int x = 1;
+
+        int index = search(a, n, x);
+
+        if (index == -1)
+            System.out.println("Element is not present in the array");
+        else
+            System.out.println("Element found at position " + index);
+    }
+}
