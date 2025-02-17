12c5c6e424f01615854521ae46a608e2788eb769
suhaspaul96 <suhasp0403@gmail.com>
2025-02-17 23:50:15 +0530

    My 11th Commit

---
+++ src/ArrayIndexSearch.java
@@ -0,0 +1,7 @@
+public class ArrayIndexSearch {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,10 @@
+/*public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println("Array elements received in Solution class:");
+        for (int num :) {
+            System.out.print(num + " ");  // Printing the array elements
+        }
+    }
+    }*/
+
