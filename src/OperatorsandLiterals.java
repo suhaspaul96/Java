87434b04654d8e7a68aad4cb6ad7aa7366d37e60
suhaspaul96 <suhasp0403@gmail.com>
2025-02-23 19:15:42 +0530

    My 14th Commit

---
+++ src/OperatorsandLiterals.java
@@ -0,0 +1,20 @@
+public class OperatorsandLiterals {
+    public static void main(String[] args) {
+        float a = 10f;
+        float b = 2.3f;
+
+        float c = a+b;
+
+        System.out.println("sum is: "+ c);
+
+        int k = 10;
+        int d  = 4;
+
+        System.out.println(k+d);
+        System.out.println(k-d);
+        System.out.println(k*d);
+        System.out.println(k/d);
+        System.out.println(k%d);
+
+    }
+}
