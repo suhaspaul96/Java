91a9643b6b0e3920e61823ed20a6fe2d870863ad
suhaspaul96 <suhasp0403@gmail.com>
2025-03-11 11:56:59 +0530

    My 21st Commit

---
+++ src/Practise/Practise0311.java
@@ -0,0 +1,18 @@
+package Practise;
+
+public class Practise0311 {
+
+    public static void main(String[] args) {
+
+        int a =10;
+        int b = 20;
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("A: "+a);
+        System.out.println("B: "+b);
+
+    }
+}
